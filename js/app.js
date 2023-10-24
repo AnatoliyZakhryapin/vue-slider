3a66156ff3de04047b2d67001ba1a3a037a1cf20
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-24 17:29:11 +0200

    ADD class active to img current

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            currentIndex: 0,
             images: [
                 {
                     image: 'img/01.webp',
